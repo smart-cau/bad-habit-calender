e56fc23f508925f6e3622b948bb6afe65d69b9a6
HyeonWooJung <1995hyunwoo@gmail.com>
2024-07-01 22:01:48 +0900

    refactor: project dir 구조 변경

---
+++ app/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+
+def create_app():
+    app = Flask(__name__)
+
+    # init extensions
+
+    # import and register blueprints
+    @app.route('/')
+    def hello_world():
+        return render_template('index.html')
+
+    # custom error handlers
+
+    return app
