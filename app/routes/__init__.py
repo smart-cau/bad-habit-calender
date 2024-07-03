6a01d49a7bca031b38ecacb3ae4735d99fe7e744
loopy-lim <loopy@jnu.ac.kr>
2024-07-03 13:46:01 +0900

    feat: add enroll regiester page

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -6,11 +6,33 @@ router = Blueprint("router", __name__)
 router.register_blueprint(auth_router)
 
 
-@router.route("/login", methods=["GET"])
-def login_page():
-    return render_template("login.html")
+@router.route("/", methods=["GET"])
+def hello_world():
+    return render_template("index.html")
 
 
-@router.route("/signup", methods=["GET"])
-def signup_page():
-    return render_template("signup.html")
+@router.route("/enroll", methods=["GET"])
+def enroll_page():
+    tempData = [
+        {"content": "개발 25시간 이상 해버리기", "_id": "1"},
+        {"content": "게임 2시간 이상 해버리기", "_id": "2"},
+        {"content": "유튜브 2시가 이상 시청 해버리기", "_id": "3"},
+    ]
+    return render_template("enroll.html", type="enroll", habits=tempData)
+
+
+@router.route("/enroll/create", methods=["GET"])
+def enroll_create_page():
+    return render_template("enroll_create.html", type="enroll_create")
+
+
+@router.route("/enroll/register", methods=["GET"])
+def enroll_register_page():
+    tempData = [
+        {"content": "개발 25시간 이상 해버리기", "_id": "1", "check": True},
+        {"content": "게임 2시간 이상 해버리기", "_id": "2", "check": True},
+        {"content": "유튜브 2시가 이상 시청 해버리기", "_id": "3", "check": False},
+    ]
+    return render_template(
+        "enroll_register.html", type="enroll_register", habits=tempData
+    )
--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,26 +3,11 @@ from flask import Blueprint, render_template
 auth_router = Blueprint("auth", __name__)
 
 
-@auth_router.route("/", methods=["GET"])
-def hello_world():
-    return render_template("index.html")
+@auth_router.route("/login", methods=["GET"])
+def login_page():
+    return render_template("login.html")
 
 
-@auth_router.route("/enroll", methods=["GET"])
-def enroll_page():
-    tempData = [
-        {"content": "개발 25시간 이상 해버리기", "_id": "1"},
-        {"content": "게임 2시간 이상 해버리기", "_id": "2"},
-        {"content": "유튜브 2시가 이상 시청 해버리기", "_id": "3"},
-    ]
-    return render_template("enroll.html", type="enroll", habits=tempData)
-
-
-@auth_router.route("/enroll/create", methods=["GET"])
-def enroll_create_page():
-    return render_template("enroll_create.html", type="enroll_create")
-
-
-@auth_router.route("/enroll/register", methods=["GET"])
-def enroll_register_page():
-    return render_template("enroll_register.html", type="enroll_register")
+@auth_router.route("/signup", methods=["GET"])
+def signup_page():
+    return render_template("signup.html")
