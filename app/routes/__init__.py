19744cb96331d619019a4f0e37a6fa46d1bac78e
HyeonWooJung <1995hyunwoo@gmail.com>
2024-07-03 20:38:31 +0900

    feat: connect habit data in enroll endpoints

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from app.routes.auth import auth_router
 from app.utils.auth_decorator import when_logged_in
-
+from flask import request
 
 router = Blueprint("router", __name__)
 router.register_blueprint(auth_router)
@@ -16,12 +16,9 @@ def home_page():
 @router.route("/enroll", methods=["GET"])
 @when_logged_in
 def enroll_page():
-    tempData = [
-        {"content": "개발 25시간 이상 해버리기", "_id": "1"},
-        {"content": "게임 2시간 이상 해버리기", "_id": "2"},
-        {"content": "유튜브 2시가 이상 시청 해버리기", "_id": "3"},
-    ]
-    return render_template("enroll.html", type="enroll", habits=tempData)
+    user_id = request.cookies.get('user_id')
+    habits = current_app.habit_service.get_habits(user_id)
+    return render_template("enroll.html", type="enroll", habits=habits)
 
 
 @router.route("/enroll/create", methods=["GET"])
@@ -33,11 +30,11 @@ def enroll_create_page():
 @router.route("/enroll/register", methods=["GET"])
 @when_logged_in
 def enroll_register_page():
-    tempData = [
-        {"content": "개발 25시간 이상 해버리기", "_id": "1", "check": True},
-        {"content": "게임 2시간 이상 해버리기", "_id": "2", "check": True},
-        {"content": "유튜브 2시가 이상 시청 해버리기", "_id": "3", "check": False},
-    ]
+    user_id = request.cookies.get('user_id')
+    date = request.args.get("currentDay")
+
+    habit_logs = current_app.habit_log_service.get_list(user_id, date)
+
     return render_template(
-        "enroll_register.html", type="enroll_register", habits=tempData
+        "enroll_register.html", type="enroll_register", habits=habit_logs
     )
